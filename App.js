fce55d1abdb8af0372c34be2a7ab13fe2ccb7f49
Kim Khemmanat <khemmanat2012@gmail.com>
2021-06-23 13:27:58 +0700

    edit the project name to alamood-depression

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Button} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button 
+        title="Learn More"
+        color="#f54242"
+      />
       <StatusBar style="auto" />
     </View>
   );
