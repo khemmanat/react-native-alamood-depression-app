bb57283b00c6e8b5856df9130e886c0f40e6a5cb
Kim Khemmanat <khemmanat2012@gmail.com>
2021-06-25 16:44:51 +0700

    Add all pages to make the redirection from the App.js

---
+++ src/action_page.js
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { Animated, Dimensions, Image,StyleSheet, Text, View, Button } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// Font Awesome Icons...
+
+export default function ActionScreen(){
+    return(
+        <View style={{ flex:1 , justifyContent: 'center',alignItems: 'center'}}>
+            <Text>Emotion!</Text>
+        </View>
+    )
+}
